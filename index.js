cdc1ff14d28a72694cbe648f23e170638707d8ee
Lejian Luo <luolejian@outlook.com>
2023-06-30 02:42:50 -0400

    edited cors url

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const mongoURI = process.env.DATABASE_URI;
 
 app.use(cors({
-    origin: ['http://localhost:3000','https://employment-client.vercel.app'],
+    origin: ['http://localhost:3000','https://employment-client.vercel.app/'],
     credentials: true,
 }));
 app.use(bodyParser.json());
